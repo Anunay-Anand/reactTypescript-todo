4f3b832fe9ff6d173f0486d56d070a92e5d5bf80
Anunay-Anand <anunayanand@gmail.com>
2021-09-26 15:17:33 +0530

    Creating base project and adding our first todo

---
+++ src/components/todos.tsx
@@ -0,0 +1,14 @@
+// Importing React and other important libraries
+import React from "react";
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
